94349b1a31afeecf5dcfc44d0e0b069a6d4ee002
Andrea Calabrese <andrea1995.c@live.com>
2024-07-30 15:05:21 +0200

    vcd-indexer: use Arc for sharing state
    Sharing the state with the VCD implemented... kind of. This should
    allow to share the status of the VCD with a future GUI application
    
    Signed-off-by: Andrea Calabrese <andrea1995.c@live.com>

--- vcd-indexer/src/lib.rs
+++ vcd-indexer/src/lib.rs
@@ -1,7 +1,9 @@
 use std::{
     collections::HashMap,
-    rc::Rc,
-    sync::mpsc::{self, Receiver},
+    sync::{
+        mpsc::{self, Receiver},
+        Arc, RwLock,
+    },
     thread,
 };
 
@@ -12,9 +14,10 @@ pub struct Configuration {
     pub separator: char,
 }
 
-pub fn index(configuration: Configuration) -> Result<VCD, String> {
+pub fn index(configuration: Configuration, vcd: Arc<RwLock<VCD>>) -> Result<(), String> {
     let (tx, rx) = mpsc::sync_channel(1000000);
-    let th = thread::spawn(move || translate_infos(rx));
+    let vcd1 = vcd.clone();
+    let th = thread::spawn(move || translate_infos(rx, vcd1));
     let reader_config = vcd_reader::Configuration {
         in_file: &configuration.in_file,
         separator: configuration.separator,
@@ -23,8 +26,8 @@ pub fn index(configuration: Configuration) -> Result<VCD, String> {
         tx.send(info).unwrap();
     });
     drop(tx);
-    let vcd = th.join().unwrap();
-    vcd
+    let res = th.join().unwrap();
+    res
 }
 
 #[derive(Debug, Default)]
@@ -41,14 +44,14 @@ pub enum Node {
 #[derive(Debug, Default)]
 pub struct Module {
     pub parent: usize,
-    pub children: HashMap<Rc<str>, Node>,
+    pub children: HashMap<Arc<str>, Node>,
 }
 
 #[derive(Debug)]
 pub struct Signal {
-    pub id: Rc<str>,
+    pub id: Arc<str>,
     pub sub_id: u16,
-    pub name: Rc<str>,
+    pub name: Arc<str>,
     pub parent_index: usize,
     pub states: Vec<State>,
 }
@@ -64,7 +67,7 @@ pub struct State {
 pub struct VCD {
     pub hierarchy: Vec<Module>,
     pub signals: Vec<Signal>,
-    pub signals_by_id: HashMap<Rc<str>, usize>,
+    pub signals_by_id: HashMap<Arc<str>, usize>,
 }
 
 impl Default for State {
@@ -122,95 +125,92 @@ impl VCD {
                 })
             })
     }
-
-    fn translate_changes(&mut self, infos: Receiver<LineInfo>) -> Result<(), String> {
-        let mut current_timestamp: i64 = -1;
-        for info in infos.into_iter() {
-            match info {
-                LineInfo::Signal(_) => unreachable!("Error: Signal declaration in initialization"),
-                LineInfo::DateInfo(_) => unreachable!("Error: Date info not expected here"),
-                LineInfo::VersionInfo(_) => unreachable!("Error: Version info not expected here"),
-                LineInfo::TimeScaleInfo(_) => {
-                    unreachable!("Error: Time scale info not expected here")
-                }
-                LineInfo::InScope(_) => unreachable!("Error: Scope definitions not expected here"),
-                LineInfo::UpScope => unreachable!("Error: Upscope not expected here"),
-                LineInfo::EndDefinitions => {
-                    unreachable!("Error: Definitions should have already ended")
-                }
-                LineInfo::EndInitializations => {}
-                LineInfo::Useless => {}
-                LineInfo::ParsingError(s) => {
-                    return Err(format!("ERROR: found unrecognized symbol: {}", s))
-                }
-
-                LineInfo::Timestamp(t) => current_timestamp = t as i64,
-                LineInfo::Change(c) => self.add_change(c, current_timestamp),
-            }
-        }
-        Ok(())
-    }
-
-    fn translate_definitions(
-        &mut self,
-        infos: Receiver<LineInfo>,
-    ) -> Result<Receiver<LineInfo>, String> {
-        let mut translator = InfoTranslator::default();
-        for info in infos.iter() {
-            match info {
-                LineInfo::Signal(s) => self.push(s, &translator),
-                LineInfo::DateInfo(s) => println!("Date: {}", s.trim().replace("$end", "").trim()),
-                LineInfo::VersionInfo(s) => {
-                    println!("Tool: {}", s.trim().replace("$end", "").trim())
-                }
-                LineInfo::TimeScaleInfo(s) => {
-                    println!("Time scale: {}", s.trim().replace("$end", "").trim())
-                }
-                LineInfo::InScope(module) => {
-                    // Gather last value index
-                    let last_value = self.hierarchy.len();
-                    // Create and push the new module
-                    let mut m = Module::default();
-                    m.parent = translator.current_module_index; // New module has current module as parent
-                    self.hierarchy.push(m);
-                    // Update old module children
-                    self.hierarchy[translator.current_module_index]
-                        .children
-                        .insert(module.into(), Node::Module(last_value));
-                    // Update current module
-                    translator.current_module_index = last_value;
-                }
-                LineInfo::UpScope => {
-                    translator.current_module_index =
-                        self.hierarchy[translator.current_module_index].parent;
-                }
-                LineInfo::ParsingError(s) => {
-                    return Err(format!("ERROR: unrecognized symbol {}", s));
-                }
-                LineInfo::EndDefinitions => break,
-                LineInfo::Useless => {}
-                LineInfo::Timestamp(t) => panic!("Unexpected timestamp: {:?}", t),
-                LineInfo::Change(c) => panic!("Unexpected change: {:?}", c),
-                LineInfo::EndInitializations => {
-                    panic!("End initializations found before the beginning!")
-                }
-            }
-        }
-        Ok(infos)
-    }
 }
 
-fn translate_infos(mut infos: Receiver<LineInfo>) -> Result<VCD, String> {
-    let mut vcd = VCD::default();
-    match vcd.translate_definitions(infos) {
+fn translate_changes(vcd: Arc<RwLock<VCD>>, infos: Receiver<LineInfo>) -> Result<(), String> {
+    let mut current_timestamp: i64 = -1;
+    for info in infos.into_iter() {
+        match info {
+            LineInfo::Signal(_) => unreachable!("Error: Signal declaration in initialization"),
+            LineInfo::DateInfo(_) => unreachable!("Error: Date info not expected here"),
+            LineInfo::VersionInfo(_) => unreachable!("Error: Version info not expected here"),
+            LineInfo::TimeScaleInfo(_) => {
+                unreachable!("Error: Time scale info not expected here")
+            }
+            LineInfo::InScope(_) => unreachable!("Error: Scope definitions not expected here"),
+            LineInfo::UpScope => unreachable!("Error: Upscope not expected here"),
+            LineInfo::EndDefinitions => {
+                unreachable!("Error: Definitions should have already ended")
+            }
+            LineInfo::EndInitializations => {}
+            LineInfo::Useless => {}
+            LineInfo::ParsingError(s) => {
+                return Err(format!("ERROR: found unrecognized symbol: {}", s))
+            }
+
+            LineInfo::Timestamp(t) => current_timestamp = t as i64,
+            LineInfo::Change(c) => vcd.write().unwrap().add_change(c, current_timestamp),
+        }
+    }
+    Ok(())
+}
+
+fn translate_definitions(
+    vcd: Arc<RwLock<VCD>>,
+    infos: Receiver<LineInfo>,
+) -> Result<Receiver<LineInfo>, String> {
+    let mut translator = InfoTranslator::default();
+    for info in infos.iter() {
+        match info {
+            LineInfo::Signal(s) => vcd.write().unwrap().push(s, &translator),
+            LineInfo::DateInfo(s) => println!("Date: {}", s.trim().replace("$end", "").trim()),
+            LineInfo::VersionInfo(s) => {
+                println!("Tool: {}", s.trim().replace("$end", "").trim())
+            }
+            LineInfo::TimeScaleInfo(s) => {
+                println!("Time scale: {}", s.trim().replace("$end", "").trim())
+            }
+            LineInfo::InScope(module) => {
+                // Gather last value index
+                let mut vcd_lock = vcd.write().unwrap();
+                let last_value = vcd_lock.hierarchy.len();
+                // Create and push the new module
+                let mut m = Module::default();
+                m.parent = translator.current_module_index; // New module has current module as parent
+                vcd_lock.hierarchy.push(m);
+                // Update old module children
+                vcd_lock.hierarchy[translator.current_module_index]
+                    .children
+                    .insert(module.into(), Node::Module(last_value));
+                // Update current module
+                translator.current_module_index = last_value;
+            }
+            LineInfo::UpScope => {
+                translator.current_module_index =
+                    vcd.write().unwrap().hierarchy[translator.current_module_index].parent;
+            }
+            LineInfo::ParsingError(s) => {
+                return Err(format!("ERROR: unrecognized symbol {}", s));
+            }
+            LineInfo::EndDefinitions => break,
+            LineInfo::Useless => {}
+            LineInfo::Timestamp(t) => panic!("Unexpected timestamp: {:?}", t),
+            LineInfo::Change(c) => panic!("Unexpected change: {:?}", c),
+            LineInfo::EndInitializations => {
+                panic!("End initializations found before the beginning!")
+            }
+        }
+    }
+    Ok(infos)
+}
+
+fn translate_infos(mut infos: Receiver<LineInfo>, vcd: Arc<RwLock<VCD>>) -> Result<(), String> {
+    match translate_definitions(vcd.clone(), infos) {
         Ok(info) => infos = info,
         Err(s) => return Err(s),
     }
-    return match vcd.translate_changes(infos) {
-        Ok(_) => Ok(vcd),
+    return match translate_changes(vcd, infos) {
+        Ok(_) => Ok(()),
         Err(s) => Err(s),
     };
 }
-
-unsafe impl Send for Node {}
-unsafe impl Send for VCD {}
