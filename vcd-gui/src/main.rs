660b8975d1b23ad9c70c841e94fe8d4c4c80123f
Andrea Calabrese <andrea.calabrese@amarulasolutions.com>
2025-12-17 15:38:36 +0100

    Improve vcd-gui logic
    Now GUI activates only if there are source and result file. Add status
    bar.
    
    Signed-off-by: Andrea Calabrese <andrea.calabrese@amarulasolutions.com>

--- vcd-gui/src/main.rs
+++ vcd-gui/src/main.rs
@@ -1,4 +1,6 @@
 use slint::SharedString;
+use std::sync::Mutex;
+use std::thread;
 use vcd_statistical_analysis::{perform_analysis, Configuration};
 
 slint::slint! {
@@ -9,9 +11,14 @@ slint::slint! {
         out property<string> out_path <=> outpath.text;
         out property<string> separator <=> sep.text;
         in property<bool> interface_enabled;
+        in property<bool> button_enabled;
+        in property<string> status_text <=> status_label.text;
 
         callback button-pressed <=> evaluate_button.clicked;
         callback browse-source-pressed <=> browse_source_button.clicked;
+        callback in-file-edited <=> inpath.edited;
+        callback out-file-edited <=> outpath.edited;
+
         preferred-width: 640px;
         preferred-height: 480px;
         min-width: 420px;
@@ -50,19 +57,22 @@ slint::slint! {
                 Row {
                     Text {
                         vertical-alignment: center;
-                        text: "Separator character (default: '<')";
+                        text: "Separator character (default: ' ')";
                     }
                     sep := LineEdit {
                         placeholder-text: "Separator";
-                        text: "<";
                         read-only: !interface_enabled;
                     }
                 }
             }
             evaluate_button := Button {
                 text: "Perform analysis";
-                enabled: interface_enabled;
+                enabled: interface_enabled && button_enabled;
                 primary: true;
+            }
+            status_label := Text {
+                vertical-alignment: center;
+                text: "";
             }
         }
     }
@@ -71,6 +81,7 @@ slint::slint! {
 fn main() {
     let window = MainWindow::new().unwrap();
     window.set_interface_enabled(true);
+    window.set_button_enabled(false);
     {
         let weak_window_source_pressed = window.as_weak();
         window.on_browse_source_pressed(move || {
@@ -89,29 +100,62 @@ fn main() {
                         path.unwrap_or("".into()).as_os_str().to_str().unwrap_or(""),
                     ));
             }
+            in_window.set_button_enabled(
+                !in_window.get_in_path().is_empty() && !in_window.get_out_path().is_empty(),
+            );
         });
     }
     {
         let weak_window = window.as_weak();
         window.on_button_pressed(move || {
-            let in_window = weak_window.upgrade().unwrap();
-            in_window.set_interface_enabled(false);
-            let in_file = in_window.get_in_path().to_string();
-            let out_file = in_window.get_out_path().to_string();
+            let in_window = Mutex::new(weak_window.upgrade().unwrap());
+            in_window.lock().unwrap().set_interface_enabled(false);
+            let in_file = in_window.lock().unwrap().get_in_path().to_string();
+            let out_file = in_window.lock().unwrap().get_out_path().to_string();
+            let out_file_txt = out_file.clone();
             let separator = in_window
+                .lock()
+                .unwrap()
                 .get_separator()
                 .to_string()
                 .chars()
                 .next()
-                .unwrap_or('<');
-            perform_analysis(Configuration {
-                in_file,
-                out_file,
-                separator,
-                use_spinner: false,
+                .unwrap_or(' ');
+            in_window
+                .lock()
+                .unwrap()
+                .set_status_text("Computing VCD statystical analysis...".into());
+            thread::spawn(move || {
+                perform_analysis(Configuration {
+                    in_file,
+                    out_file,
+                    separator,
+                    use_spinner: false,
+                });
             });
-            in_window.set_interface_enabled(true);
+            in_window.lock().unwrap().set_interface_enabled(true);
+            in_window
+                .lock()
+                .unwrap()
+                .set_status_text(format!("VCD analyzed. Result in {}", out_file_txt).into());
         });
     }
+    {
+        let weak_window = window.as_weak();
+        window.on_in_file_edited(move |text| {
+            weak_window.upgrade().unwrap().set_button_enabled(
+                !text.is_empty() && !weak_window.upgrade().unwrap().get_out_path().is_empty(),
+            );
+        });
+    }
+    {
+        let weak_window = window.as_weak();
+        window.on_out_file_edited(move |text| {
+            weak_window.upgrade().unwrap().set_button_enabled(
+                !text.is_empty() && !weak_window.upgrade().unwrap().get_in_path().is_empty(),
+            );
+        });
+    }
+
     window.run().unwrap();
 }
