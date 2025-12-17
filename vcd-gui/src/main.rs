980b1702be300ef4767276f8e881904ecb88ab84
Andrea Calabrese <andrea.calabrese@amarulasolutions.com>
2025-12-17 11:49:55 +0100

    Improved VCD GUI
    Signed-off-by: Andrea Calabrese <andrea.calabrese@amarulasolutions.com>

--- vcd-gui/src/main.rs
+++ vcd-gui/src/main.rs
@@ -7,6 +7,7 @@ slint::slint! {
         out property<string> in_path <=> inpath.text;
         out property<string> out_path <=> outpath.text;
         out property<string> separator <=> sep.text;
+        in property<bool> interface_enabled;
 
         callback button-pressed <=> evaluate_button.clicked;
         preferred-width: 640px;
@@ -17,45 +18,58 @@ slint::slint! {
         VerticalLayout {
             padding: 8px;
             alignment: center;
-                GroupBox {
+            spacing: 32px;
+            GridLayout {
+                spacing: 32px;
+                Row {
                     Text {
                         vertical-alignment: center;
-                        text: "Input VCD file path  ";
+                        text: "Input VCD file path";
+
                     }
                     inpath := LineEdit {
                         placeholder-text: "Path";
+                        read-only: !interface_enabled;
                     }
                 }
-                GroupBox {
+                Row {
                     Text {
                         vertical-alignment: center;
-                        text: "Output file path  ";
+                        text: "Output file path";
                     }
                     outpath := LineEdit {
                         placeholder-text: "Path";
+                        read-only: !interface_enabled;
                     }
                 }
-                GroupBox {
+                Row {
                     Text {
                         vertical-alignment: center;
-                        text: "Separator character  ";
+                        text: "Separator character (default: '<')";
                     }
                     sep := LineEdit {
                         placeholder-text: "Separator";
                         text: "<";
+                        read-only: !interface_enabled;
                     }
                 }
-                evaluate_button := Button {
-                    text: "Perform analysis";
-                }
+            }
+            evaluate_button := Button {
+                text: "Perform analysis";
+                enabled: interface_enabled;
+                primary: true;
+            }
         }
     }
 }
+
 fn main() {
     let window = MainWindow::new().unwrap();
     let weak_window = window.as_weak();
+    window.set_interface_enabled(true);
     window.on_button_pressed(move || {
         let in_window = weak_window.upgrade().unwrap();
+        in_window.set_interface_enabled(false);
         let in_file = in_window.get_in_path().to_string();
         let out_file = in_window.get_out_path().to_string();
         let separator = in_window
@@ -63,14 +77,14 @@ fn main() {
             .to_string()
             .chars()
             .next()
-            .unwrap();
-
+            .unwrap_or('<');
         perform_analysis(Configuration {
             in_file,
             out_file,
             separator,
             use_spinner: false,
         });
+        in_window.set_interface_enabled(true);
     });
     window.run().unwrap();
 }
